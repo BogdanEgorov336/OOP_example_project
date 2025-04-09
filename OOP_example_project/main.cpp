c4c4402814161a5a4c53d3908e9f96d97597f6a2
Egorov Bogdan <Egorov Bogdan>
2025-04-09 17:58:11 +0300

    constructors (4) and destrusctor added

--- OOP_example_project/main.cpp
+++ OOP_example_project/main.cpp
@@ -1,51 +1,22 @@
 #include "teacher.h"
 
-void change(Student st) {
-
-	st.name = "Vova";
-	st.age = 15;
-	st.avg_mark = 10;
-	st.alive = false;
-
-}
-
 int main() {
 
-	Student* list = nullptr;
+	Student student1{};
+	/*Student student2("Alex");
+	Student student3("Vlad", 13, 10, true);
+	Student student4(student2);*/
 
-	int size;
-	cout << "How may students is there: ";
-	cin >> size;
+	Student student2{ "Alex" };
+	//Student student3{ "Vlad", 13, 10, true };
 
-	list = new Student[size];
+	Student student3{ "Vlad", 13};
+	Student student4{ student3 };
 
-	cout << "Input students data:\n";
-	for (int i = 0; i < size; i++) {
-
-		cout << "STUDENT " << i + 1
-			<< "\nName: ";
-		cin >> list[i].name;
-
-		cout << "Age: ";
-		cin >> list[i].age;
-
-		cout << "AVG mark: ";
-		cin >> list[i].avg_mark;
-
-		cout << "Is alive (y/n): ";
-		char answer;
-		cin >> answer;
-
-		list[i].alive = islower(answer) == 'y';
-	}
-
-	Teacher teacher;
-
-	Student result = teacher.GetBestStudent(list, size);
-
-	string msg = result.alive ? "Best student is " + result.name : "No such ones.";
-
-	cout << msg << endl;
+	cout << student1.ToString() << endl;
+	cout << student2.ToString() << endl;
+	cout << student3.ToString() << endl;
+	cout << student4.ToString() << endl;
 
 	return 0;
 }
--- OOP_example_project/student.h
+++ OOP_example_project/student.h
@@ -11,7 +11,55 @@ public:
 	double avg_mark;
 	bool alive;
 
-	string getString() {
+	//default constructor (without any arguments)
+	Student() {
+		
+		cout << "...default-constructor" << endl;
+
+		name = "No name";
+		age = 13;
+		avg_mark = 4.0;
+		alive = true;
+	}
+
+	//constructor with arguments
+	Student(string nm) {
+
+		cout << "...constructor with arguments" << endl;
+		name = nm;
+		age = 13;
+		avg_mark = 4.0;
+		alive = true;
+	}
+
+	//canonical constructor
+	Student(string nm, int a, double mark, bool alv) {
+
+		cout << "...canonical-constructor with arguments" << endl;
+		name = nm;
+		age = a;
+		avg_mark = mark;
+		alive = alv;
+	}
+
+	//copy-constructor
+	Student(const Student& student) {
+
+		cout << "...copy-constructor with arguments" << endl;
+		name = student.name;
+		age = student.age;
+		avg_mark = student.avg_mark;
+		alive = student.alive;
+	}
+
+	//destructor
+	~Student() {
+
+		cout << "destructor..." << endl;
+
+	}
+
+	string ToString() {
 
 		return "Name: " + name
 			+ "\nAge: " + to_string(age)
