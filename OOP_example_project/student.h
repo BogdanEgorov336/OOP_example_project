35b215504e936f4038c763d7d93e068eedf49b5f
Egorov Bogdan <Egorov Bogdan>
2025-04-23 18:48:36 +0300

    update

--- OOP_example_project/student.h
+++ OOP_example_project/student.h
@@ -15,22 +15,22 @@ public:
 	//default constructor (without any arguments)
 	Student() : Student("No name", 13, 10, true) {
 		
-		cout << "...default-constructor" << endl;
+		//cout << "...default-constructor" << endl;
 	}
 
 	//constructor with arguments
-	Student(string nm) : Student(nm, 13, 0, 4.0, true) {
+	Student(string nm) : Student(nm, 13, 0, true) {
 
-		cout << "...constructor with arguments" << endl;
+		//cout << "...constructor with arguments" << endl;
 		name = nm;
 		age = 13;
 		alive = true;
 	}
 
 	//canonical constructor
-	Student(string name, int age, int countMark, double avg_mark, bool alive) {
+	Student(string name, int age, int countMark, bool alive) {
 
-		cout << "...canonical-constructor with arguments" << endl;
+		//cout << "...canonical-constructor with arguments" << endl;
 		this->name = name;
 		this->age = age;
 		this->alive = alive;
@@ -46,17 +46,12 @@ public:
 	Student(const Student& student) : Student(student.name, student.age,
 		student.countMark, student.alive) {
 
-		/*cout << "...copy-constructor with arguments" << endl;
-		name = student.name;
-		age = student.age;
-		countMark = student.countMark;
-		alive = student.alive;*/
 	}
 
 	//destructor
 	~Student() {
 
-		cout << "destructor..." << endl;
+		//cout << "destructor..." << endl;
 
 		if (marks) { 
 			delete[] marks;
