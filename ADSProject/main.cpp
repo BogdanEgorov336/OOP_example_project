78e92d7a847cb0ff813312f2849e15414ffa3f70
Egorov Bogdan <Egorov Bogdan>
2025-05-14 18:46:46 +0300

    queue added

--- ADSProject/main.cpp
+++ ADSProject/main.cpp
@@ -1,3 +1,27 @@
-#include "ArrayList.h"
+#include "Queue.h"
 using namespace std;
 
+int main() {
+
+	Queue queue;
+
+	cout << "Enter elements (\"0\" for exit): ";
+	int element;
+
+	do {
+		cin >> element;
+		if (element != 0) {
+			queue.enqueue(element);
+		}
+	} while (element != 0);
+
+	cout << "Queue: ";
+	
+	while (!queue.isEmpty()) {
+
+		cout << queue.dequeue() << " ";
+	}
+
+	system("pause");
+	return 0;
+}
